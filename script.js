0a9ae871e2c540c47a73c4a19982d31e78c5158b
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-10 23:03:00 +0100

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+"use strict";
 const array = [];
 let counter = 0;
 setInterval(() => {
     counter++;
+    //added the counter at the beginning of the array
     array.unshift(counter);
+    //created condition so it doesn't go higher than 9
     if (array.length > 9) {
+        //removes the element at the end of the array
         array.pop();
     }
     console.log(array);
